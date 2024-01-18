925090e66bbf1f27d1b2d2e72a15924825216859
Kasper Hjort Jæger <khj@lasertryk.dk>
2024-01-18 15:59:53 +0100

    removed unnessecary properties

--- kornelius/MVVM/Model/Issue.cs
+++ kornelius/MVVM/Model/Issue.cs
@@ -17,11 +17,9 @@ namespace kornelius.Model
     public class Fields
     {
         public Issuetype issuetype { get; set; }
-        public object[] components { get; set; }
         public int? timespent { get; set; }
         public int? timeoriginalestimate { get; set; }
         public string description { get; set; }
-        public Project project { get; set; }
         public int? aggregatetimespent { get; set; }
         public double? AggregateTimeSpentHours
         {
@@ -39,39 +37,19 @@ namespace kornelius.Model
             }
         }
         public Resolution resolution { get; set; }
-        public string customfield_10005 { get; set; }
-        public object customfield_10401 { get; set; }
-        public object customfield_10402 { get; set; }
-        public string customfield_10009 { get; set; }
         public int? aggregatetimeestimate { get; set; }
         public DateTime? resolutiondate { get; set; }
-        public int? workratio { get; set; }
         public string summary { get; set; }
-        public object lastViewed { get; set; }
-        public Watches watches { get; set; }
-        public Creator creator { get; set; }
         public object[] subtasks { get; set; }
         public DateTime created { get; set; }
-        public Reporter reporter { get; set; }
-        public Customfield_10000[] customfield_10000 { get; set; }
-        public Aggregateprogress aggregateprogress { get; set; }
         public Priority priority { get; set; }
-        public object customfield_10001 { get; set; }
-        public string customfield_10100 { get; set; }
-        public object customfield_10200 { get; set; }
-        public string[] labels { get; set; }
-        public string[] customfield_10004 { get; set; }
-        public object customfield_10400 { get; set; }
-        public object environment { get; set; }
         public int? timeestimate { get; set; }
         public int? aggregatetimeoriginalestimate { get; set; }
         public string duedate { get; set; }
         public Progress progress { get; set; }
         public object[] issuelinks { get; set; }
-        public Votes votes { get; set; }
         public Assignee assignee { get; set; }
         public DateTime updated { get; set; }
-        public Status status { get; set; }
         public override string ToString()
         {
             return summary;
@@ -99,32 +77,6 @@ namespace kornelius.Model
         public int avatarId { get; set; }
     }
 
-    public class Project
-    {
-        public string self { get; set; }
-        public string id { get; set; }
-        public string key { get; set; }
-        public string name { get; set; }
-        public Avatarurls avatarUrls { get; set; }
-        public Projectcategory projectCategory { get; set; }
-    }
-
-    public class Avatarurls
-    {
-        public string _48x48 { get; set; }
-        public string _24x24 { get; set; }
-        public string _16x16 { get; set; }
-        public string _32x32 { get; set; }
-    }
-
-    public class Projectcategory
-    {
-        public string self { get; set; }
-        public string id { get; set; }
-        public string description { get; set; }
-        public string name { get; set; }
-    }
-
     public class Resolution
     {
         public string self { get; set; }
@@ -133,52 +85,6 @@ namespace kornelius.Model
         public string name { get; set; }
     }
 
-    public class Watches
-    {
-        public string self { get; set; }
-        public int watchCount { get; set; }
-        public bool isWatching { get; set; }
-    }
-
-    public class Creator
-    {
-        public string self { get; set; }
-        public string name { get; set; }
-        public string key { get; set; }
-        public string emailAddress { get; set; }
-        public Avatarurls1 avatarUrls { get; set; }
-        public string displayName { get; set; }
-        public bool active { get; set; }
-        public string timeZone { get; set; }
-    }
-
-    public class Avatarurls1
-    {
-        public string _48x48 { get; set; }
-        public string _24x24 { get; set; }
-        public string _16x16 { get; set; }
-        public string _32x32 { get; set; }
-    }
-
-    public class Reporter
-    {
-        public string self { get; set; }
-        public string name { get; set; }
-        public string key { get; set; }
-        public string emailAddress { get; set; }
-        public Avatarurls2 avatarUrls { get; set; }
-        public string displayName { get; set; }
-        public bool active { get; set; }
-        public string timeZone { get; set; }
-    }
-
-    public class Avatarurls2
-    {
-        public string _48x48 { get; set; }
-        public string _24x24 { get; set; }
-        public string _16x16 { get; set; }
-        public string _32x32 { get; set; }
-    }
 
     public class Aggregateprogress
     {
@@ -202,12 +108,6 @@ namespace kornelius.Model
         public int percent { get; set; }
     }
 
-    public class Votes
-    {
-        public string self { get; set; }
-        public int votes { get; set; }
-        public bool hasVoted { get; set; }
-    }
 
     public class Assignee
     {
@@ -215,19 +115,11 @@ namespace kornelius.Model
         public string name { get; set; }
         public string key { get; set; }
         public string emailAddress { get; set; }
-        public Avatarurls3 avatarUrls { get; set; }
         public string displayName { get; set; }
         public bool active { get; set; }
         public string timeZone { get; set; }
     }
 
-    public class Avatarurls3
-    {
-        public string _48x48 { get; set; }
-        public string _24x24 { get; set; }
-        public string _16x16 { get; set; }
-        public string _32x32 { get; set; }
-    }
 
     public class Status
     {
@@ -247,12 +139,4 @@ namespace kornelius.Model
         public string colorName { get; set; }
         public string name { get; set; }
     }
-
-    public class Customfield_10000
-    {
-        public string self { get; set; }
-        public string value { get; set; }
-        public string id { get; set; }
-    }
-
 }
